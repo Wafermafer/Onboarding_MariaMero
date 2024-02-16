1988472e84807f9d5d1e098a35c3362856762c84
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-02-15 23:15:42 -0300

    Add: Functions Exercises. Level: Low

---
+++ conditionals/app.js
@@ -0,0 +1,80 @@
+
+/* Ejercicio 1 */
+/*let num1 = 4
+let num2 = 5
+
+if( num1 > num2){
+    console.log (num1)
+}else{
+    console.log(num2)
+}*/
+
+/* Ejercicio 2 */
+/*let num1 = 3
+let num2 = 3
+
+if( num1 > num2){
+    console.log (num1)
+}else if (num2>num1){
+    console.log(num2)
+}else{
+    console.log("The numbers are equal ")
+}*/
+
+
+/* Ejercicio 3 */
+/*let num1 = 3
+let num2 = 3
+
+if( num1 == num2){
+    console.log ("The numbers are equal ")
+
+}else{
+    console.log("The numbers are different")
+}*/
+
+/* Ejercicio 4 */
+
+/*let date1 = "2018-9-2"
+
+let date2 = "2023-7-5"
+
+if( date1 > date2){
+    console.log ("The date" + " " + date1 + " " + "is greater than the date" + " " + date2 )
+}else if (date2>date1){
+    console.log("The date" + " " + date2 + " " + "is greater than the date" + " " + date1)
+}else{
+    console.log("The dates are the same")
+}*/
+
+/*Ejercicio 5*/
+
+/*let date1 = "2024-10-2"
+
+let date2 = "2030-19-6"
+
+if( date1 > date2){
+    console.log ("The date" + " " + date1 + " " + "is greater than the date" + " " + date2 )
+}else if (date2>date1){
+    console.log("The date" + " " + date2 + " " + "is greater than the date" + " " + date1)
+}else{
+    console.log("The dates are the same")
+}*/
+
+
+/*Ejercicio 6 */
+
+let number1 = 5
+
+let number2 = 7
+
+let number3 = 9
+
+if( number1 > number2 && number1 > number3){
+    console.log ("The number" + " " + number1 + " " + "is the largest" )   
+} else if (number2 > number1 && number2 > number3){
+    console.log ("The number" + " " + number2 + " " + "is the largest" )   
+}else{
+    console.log ("The number" + " " + number3 + " " + "is the largest" )
+}
+
