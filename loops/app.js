1076dc67f914acbd32df14529c1500cdd5ff462f
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-02-16 09:25:07 -0300

    Add: Loops Exercises. Level: Low

---
+++ loops/app.js
@@ -0,0 +1,82 @@
+/*Ejercicio 1*/
+
+/*var number = parseInt(prompt("Enter a number between 1 and 100:"));
+
+if (number >= 1 && number <= 100) {
+    
+    for (var i = number; i >= 0; i--) {
+        console.log(i)
+    }
+} else {
+    console.log("Invalid input. Please enter a number between 1 and 100.")
+}*/
+
+
+/*Ejercicio 2*/
+/*var number = parseInt(prompt("Ingrese un número entre 1 y 10:"));
+
+if (number >= 1 && number <= 10) {
+    for (var i = 1; i <= 10; i++) {
+        var result = i * number
+        console.log(i + "x" + number + "=" + result)
+    }
+} else {
+    console.log("Entrada inválida. Por favor ingrese un número entre 1 y 10.")
+}*/
+
+/*Ejercicio 3*/
+/*var sum = 0
+
+var input = prompt("Ingrese un número (ingrese 0 para finalizar):")
+
+while (input !== "0") {
+    var number = parseInt(input)
+    sum += number
+    input = prompt("Ingrese un número (ingrese 0 para finalizar):")
+}
+console.log("La suma de los números ingresados es: " + sum) */
+
+/*Ejercicio 4 */
+
+/*let sum = 0;
+
+let input;
+do {
+    input = prompt("Enter a number (enter 0 to finish):")
+    const number = parseInt(input)
+    sum += number
+} while (input !== "0")
+
+console.log(`The sum of the numbers entered is: ${sum}`)*/
+
+/*Ejercicio 5 */
+
+/*const person = {
+    name: "Paula",
+    lastName: "Acosta",
+    origin: "Ecuador",
+    studies: "Profesora",
+    age: 30,
+};
+
+for (let key in person) {
+    if (person.hasOwnProperty(key)) {
+        console.log(key)
+    }
+}*/
+
+/*Ejercicio 6*/
+
+/*const person = {
+    name: "Paula",
+    lastName: "Acosta",
+    origin: "Ecuador",
+    studies: "Profesora",
+    age: 30,
+};
+
+for (let key in person) {
+    if (person.hasOwnProperty(key)) {
+        console.log(person[key]);
+    }
+}*/
