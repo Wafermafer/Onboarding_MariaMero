135230d6287a134afdb154a73d53f9f87c5e4564
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-02-15 23:13:53 -0300

    Add: JS Basic Exercises

---
+++ js_Basic/app.js
@@ -0,0 +1,37 @@
+//Ejercicios JS Basic Exercise
+
+/* Ejercicio 1*/
+
+let myName = "Maria"
+
+console.log (myName)
+
+/* Ejercicio 2*/
+
+let myLastName = "Mero"
+
+console.log (myLastName)
+
+/* Ejercicio 3 */
+
+let myAge = "24"
+
+console.log (myAge)
+
+/*Ejercicio 4*/
+
+let myPet = "Tonky"
+
+console.log (myPet)
+
+/* Ejercicio 5*/
+
+let petAge = "6"
+
+console.log (petAge) 
+
+/*Ejercicio 6*/
+
+let fullName = myName +  " " + myLastName
+
+console.log (fullName)
