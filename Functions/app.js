56f9d3dd44a971d8c685d405e655dd656adcd480
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-02-15 23:20:18 -0300

    Add: Functions Exercises. Level: Low

---
+++ Functions/app.js
@@ -0,0 +1,54 @@
+/*Ejercicio 1 */
+
+/*function add() {
+    console.log(100 * 20)
+}*/
+
+/*Ejercicio 2*/
+/*function add() {
+    console.log(100 * 20)
+}
+add();*/
+
+/* Ejercicio 3*/
+
+/*function add(number1, number2) {
+    console.log(number1 * number2)
+}
+add(100, 20)*/
+
+
+/*Ejercicio 4*/
+/*let number1 = 100
+let number2 = 20
+
+function add(number1, number2) {
+    console.log(number1 * number2)
+}
+add(number1, number2)*/
+
+
+/*Ejercicio 5*/
+/*let number1 = 100
+let number2 = 20
+
+function add(number1, number2) {
+    console.log(number1 * number2)
+
+    
+    return number1 * number2
+}
+add(number1, number2)*/
+
+
+/*Ejercicio 6*/
+/*let number1 = 100
+let number2 = 20
+
+var add = function (number1, number2) {
+    console.log(number1 * number2)
+
+    return number1 * number2
+}
+add(number1, number2)*/
+
