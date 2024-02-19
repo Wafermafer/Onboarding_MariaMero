bca2ec3cd4bd582ed2bfab07b7ab58df66605673
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-02-19 09:37:30 -0300

    JS Basic Exercises. Level: Medium

--- introJS/app.js
+++ introJS/app.js
@@ -1,37 +1,108 @@
-//Ejercicios JS Basic Exercise
+//Ejercicios JS Basic Exercise. Level: Low
 
 /* Ejercicio 1*/
 
-let myName = "Maria"
+/*let myName = "Maria"
 
-console.log (myName)
+console.log (myName)*/
 
 /* Ejercicio 2*/
 
-let myLastName = "Mero"
+/*let myLastName = "Mero"
 
-console.log (myLastName)
+console.log (myLastName)*/
 
 /* Ejercicio 3 */
 
-let myAge = "24"
+/*let myAge = "24"
 
-console.log (myAge)
+console.log (myAge)*/
 
 /*Ejercicio 4*/
 
-let myPet = "Tonky"
+/*let myPet = "Tonky"
 
-console.log (myPet)
+console.log (myPet)*/
 
 /* Ejercicio 5*/
 
-let petAge = "6"
+/*let petAge = "6"
 
-console.log (petAge) 
+console.log (petAge)*/
 
 /*Ejercicio 6*/
 
-let fullName = myName +  " " + myLastName
+/*let fullName = myName +  " " + myLastName
 
-console.log (fullName)
+console.log (fullName)*/
+
+
+//Ejercicios JS Basic Exercise. Level: Medium
+
+/* Ejercicio 1*/
+
+/*let miEdad = 22
+let edadMascota = 6
+
+// Operaciones aritméticas
+let sumaEdades = miEdad + edadMascota
+let restaEdades = miEdad - edadMascota
+let productoEdades = miEdad * edadMascota
+let divisionEdades = miEdad / edadMascota
+
+// Imprimir los resultados
+console.log(`La suma de las edades es: ${sumaEdades}`)
+console.log(`La resta de las edades es: ${restaEdades}`)
+console.log(`El producto de las edades es: ${productoEdades}`)
+console.log(`La división de las edades es: ${divisionEdades}`) */
+
+/* Ejercicio 2*/
+
+/*let student = {
+    nombre: "Mafer",
+    edad: 22,
+    grado: "Universitario",
+    promedio: 8,
+    trabaja: true
+};
+
+console.table(student);
+
+// Mostrar cada propiedad del objeto por separado
+console.log("Nombre del estudiante:", student.nombre);
+console.log("Edad del estudiante:", student.edad);
+console.log("Grado del estudiante:", student.grado);
+console.log("Promedio del estudiante:", student.promedio);
+console.log("¿Está trabajando el estudiante?", student.trabaja); */
+
+/* Ejercicio 3 */
+
+/*let pet = {
+    nombre: "Tonky",
+    especie: "Perro",
+    edad: 6,
+    color: "Marrón con blanco",
+    vacunado: true
+};
+
+console.table(pet);
+
+// Mostrar cada propiedad del objeto por separado
+console.log("Nombre de la mascota:", pet.nombre);
+console.log("Especie de la mascota:", pet.especie);
+console.log("Edad de la mascota:", pet.edad);
+console.log("Color de la mascota:", pet.color);
+console.log("¿Está vacunada la mascota?", pet.vacunado);*/
+
+/*Ejercicio 4*/
+
+/*let frutas = ["Fresa", "Banana", "Naranja", "Uva", "Mora"];
+
+console.log("Array completo de frutas:", frutas);
+
+// Mostrar cada elemento del array por separado en la consola
+console.log("Primera fruta:", frutas[0]);
+console.log("Segunda fruta:", frutas[1]);
+console.log("Tercera fruta:", frutas[2]);
+console.log("Cuarta fruta:", frutas[3]);
+console.log("Quinta fruta:", frutas[4]);*/
